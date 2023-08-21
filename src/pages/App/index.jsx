35e8cf557c2817cb66b2aeb98453fb43110edea7
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-20 21:15:51 -0500

    5 se crean compoentes y se intala  el router dom

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import Home from '../home';
+import NotFound from '../NotFound';
+import MyAccount from '../MyAccount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import SignIn from '../SignIn';
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: 'my-account', element: <MyAccount /> },
+    { path: 'my-order', element: <MyOrder /> },
+    { path: 'my-orders', element: <MyOrders /> },
+    { path: 'sign-in', element: <SignIn /> },
+    { path: '/', element: <Home /> },
+    { path: '/*', element: <NotFound /> },
+  ]);
+  return routes;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
