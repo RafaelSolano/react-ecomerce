bfd698a969ad049cf41aa49e72c687187130b070
Rafael Solano <rafaelsolanol007@gmail.com>
2023-08-22 10:09:12 -0500

    17 se crea el carrito de compras

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,31 +1,36 @@
-
 import { HiXMark } from 'react-icons/hi2';
 
 import './styles.css';
 import { ShoppingCartContex } from '../../context';
 import { useContext } from 'react';
+import OrderCard from '../OrderCard';
 
 const CheckoutSideMenu = () => {
-  const {
-    isCheckoutSideMenuopen,
-    closeCheckoutSideMenu,
-    productToShow: { title, image, description, price },
-  } = useContext(ShoppingCartContex);
+  const { cartProducts, isCheckoutSideMenuopen, closeCheckoutSideMenu } =
+    useContext(ShoppingCartContex);
 
   return (
     <aside
       className={`${
         isCheckoutSideMenuopen ? 'flex' : 'hidden'
-      } product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white p-5 overflow-x-scroll`}>
-      <div className=' flex justify-between items-center '>
+      } product-detail flex flex-col fixed right-0 border border-black rounded-lg  bg-white  overflow-x-scroll z-10 `}>
+      <div className=' flex justify-between items-center fixed w-full bg-white rounded-lg p-2 '>
         <h2 className=' font-medium text-xl'>My order</h2>
         <HiXMark
           onClick={() => closeCheckoutSideMenu()}
           className='cursor-pointer h-6 w-6 hover:text-slate-600 fixed right-10'
         />
       </div>
-
-
+      <div className='mt-10 p-4'>
+        {cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
     </aside>
   );
 };
---
+++ src/components/OrderCard/index.jsx
@@ -0,0 +1,28 @@
+import { HiXMark } from 'react-icons/hi2';
+
+const OrderCard = (props) => {
+
+  const {  title, image, price} = props;
+  return (
+    <div className='flex justify-between items-center my-4 '>
+      <figure className='w-20 h-20 ' >
+        <img className='w-full h-full rounded-lg object-contain' src={image} alt={title} />
+        
+      </figure  >
+        <p className='text-sm font-light'>{title}</p>
+    
+      
+      <div className='flex items-center gap-2'>
+        <p className='text-lg font-medium'>${price}</p>
+        <p>
+          <HiXMark
+            onClick={()=>console.log('borrar item')}
+            className='h-6 w-6 text-black cursor-pointer' />
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default OrderCard
